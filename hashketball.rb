6b2d778cb05dc17511ede88f5ed21b3dc06c703b
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-21 04:43:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,6 +201,7 @@ def big_shoe_rebounds
   big_shoe_counter = {:shoe => 0, :rebounds => 0}
   game_hash.each do |team, teamHash|
     teamHash[:players].each do |hash|
+      binding.pry
       if [hash][:shoe] > big_shoe_counter[:shoe] 
         big_shoe_counter[:name] = hash[:shoe] && big_shoe_counter[:rebounds] = hash[:rebounds]
       end
