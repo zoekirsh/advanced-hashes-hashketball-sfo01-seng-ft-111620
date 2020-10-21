df31f2f65674fea761e32a3781c541fe6de6a98d
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-21 04:24:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -179,9 +179,10 @@ def player_numbers(team_name)
   
   all_player_numbers = []
   game_hash.each do |team, teamHash|
-    if teamHash[:team_name] == team_name do 
+    if teamHash[:team_name] == team_name 
       teamHash[:players].each do |hash|
         all_player_numbers << hash[:number]
+        binding.pry
       end
   end
   return all_player_numbers.sort 
